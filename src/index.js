f35fcaa0949aa869f1144ca8347a7d0afaab06e2
theNickola <kurilchik.nv@gmail.com>
2023-01-10 10:23:40 +0300

    feat: completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    n=Math.abs(n);
+    let num=n%10;
+    n=Math.floor(n/10);
+    while(n!=0)
+        {
+            num = num*10+n%10;
+            n=Math.floor(n/10);
+        }
+    return num;
 }
